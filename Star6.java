358edd74754330e52b8d551f33b49d95c88ee15e
asitkr <krsinhasaurabh@gmail.com>
2022-07-10 09:09:10 +0530

    first commit

---
+++ Star6.java
@@ -0,0 +1,30 @@
+package com.star;
+
+import java.util.Scanner;
+
+public class Star6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=n-1; j>=i; j--) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
